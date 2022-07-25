bd38ee7055b1d596c7c10da6011d801062c22537
Gerren <g.ballard23@outlook.com>
2022-07-25 12:19:18 -0500

    update

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import '../App.css';
+import { Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route>
+    
+        </Route> 
+        <Route>
+          
+        </Route>
+        <Route>
+          
+        </Route>
+        <Route>
+          
+        </Route>
+        <Route>
+          
+        </Route>
+        <Route>
+          
+        </Route>
+        <Route>
+          
+        </Route>
+      </Switch>
+
+      
+    </div>
+  );
+}
+
+export default App;
