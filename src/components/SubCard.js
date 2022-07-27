cf740e5b6834beb0d4340352b703feac51542f91
Gerren <g.ballard23@outlook.com>
2022-07-26 21:09:53 -0500

    update

--- src/components/SubCard.js
+++ src/components/SubCard.js
@@ -8,7 +8,14 @@ function SubCard({subs, coms}){
         <div className="subcard">
             <h1><strong>{subs.name}</strong></h1>
             <div>
-                <h3>net_worth:{subs.net_worth}</h3>
+                <h3>est.{subs.established}</h3>
+            </div>
+            <div>
+                <ul>
+                    <li><strong>Net Worth:{subs.net_worth}</strong></li>
+                    <li><strong>Games: {subs.games.length}</strong></li>
+                    <li><strong>Parent Company:</strong></li>
+                </ul>
             </div>
            
 
