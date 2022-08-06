4d9e06c164451511ca530a9999b386341d33346a
Gerren <g.ballard23@outlook.com>
2022-08-05 23:19:28 -0500

    update

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -4,21 +4,16 @@ function GameCard({games, subs}){
 
     const developer = subs.filter((x) => (x.id === games.subsidiary_id))
     const devName = developer.map((x) => (x.name))
-   
-    console.log(games)
 
     return (
     <div>
         <div>
-        <h1>{games.title}</h1>
+            <h1>{games.title}</h1>
         </div>
         <div>
-        <h3>Released:{games.released}</h3>
-        <h3>Developer:{devName}</h3>
+            <h3>Released:{games.released}</h3>
+            <h3>Developer:{devName}</h3>
         </div>
-
-
-
     </div>
     )
 }
